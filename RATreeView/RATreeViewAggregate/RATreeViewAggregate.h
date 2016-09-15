cfd0e9b36335b81b73a4c95b86a48abb4680d4d2
priyarajagopal <priya.rajagopal@invicara.com>
2016-09-15 14:16:46 -0400

    Added aggregate framework target to generate framework for cross platform:

---
+++ RATreeView/RATreeViewAggregate/RATreeViewAggregate.h
@@ -0,0 +1,19 @@
+//
+//  RATreeViewAggregate.h
+//  RATreeViewAggregate
+//
+//  Created by Priya Rajagopal on 9/15/16.
+//  Copyright © 2016 Rafal Augustyniak. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+//! Project version number for RATreeViewAggregate.
+FOUNDATION_EXPORT double RATreeViewAggregateVersionNumber;
+
+//! Project version string for RATreeViewAggregate.
+FOUNDATION_EXPORT const unsigned char RATreeViewAggregateVersionString[];
+
+// In this header, you should import all the public headers of your framework using statements like #import <RATreeViewAggregate/PublicHeader.h>
+
+
